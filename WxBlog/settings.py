730035c68b5e31305b3ccb207f40100be24d2771
root <root@DESKTOP-1B4C9Q1.localdomain>
2020-08-03 20:41:04 +0800

    准备部署

--- WxBlog/settings.py
+++ WxBlog/settings.py
@@ -200,13 +200,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
 
+# 静态资源
+STATIC_URL = '/static/'
 # 设置静态文件 文件夹
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_files"),
 )
 
+# 动态资源
 # 媒体文件收集
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
